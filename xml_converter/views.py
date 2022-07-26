3fb0c29d784a660f7957979003a4f85ee6a45a26
Gustavo Lopes <gustavo.ldbrito@mundodevops.com>
2022-07-26 14:21:13 -0300

    Avoiding error when not sending files

--- xml_converter/views.py
+++ xml_converter/views.py
@@ -4,11 +4,14 @@ from xml_converter.xml_conversion import XMLConverterToDict, InvalidXMLError
 
 def upload_page(request):
     if request.method == 'POST':
-        xml_file = request.FILES['file']
-        try:
-            xml_converter = XMLConverterToDict(xml_file)
-            return JsonResponse(xml_converter.to_dict())
-        except InvalidXMLError as e:
-            return JsonResponse({'error': str(e)}, status=400)
+        xml_file = request.FILES.get('file')
+        if not xml_file:
+            return JsonResponse({'error': 'No file provided'})
+        else:
+            try:
+                xml_converter = XMLConverterToDict(xml_file)
+                return JsonResponse(xml_converter.to_dict())
+            except InvalidXMLError as e:
+                return JsonResponse({'error': str(e)}, status=400)
 
     return render(request, "upload_page.html")
